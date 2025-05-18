dc4f67bcebde649c986d0a7b5ed4ff96ec404216
Konduru Priyanka <139795477+kondurupriyanka@users.noreply.github.com>
2025-05-18 13:05:40 +0530

    Updated plants.ts

--- src/data/plants.ts
+++ src/data/plants.ts
@@ -7,23 +7,23 @@ export const plants: Plant[] = [
     scientificName: 'Ocimum sanctum',
     family: 'Lamiaceae',
     commonNames: ['Tulsi', 'Sacred Basil'],
-    image: 'https://img.freepik.com/premium-photo/basil-flower_55883-4019.jpg',
+    image: 'https://img.freepik.com/premium-photo/basil-flower_55883-4019.jpg?w=996',
     medicinalProperties: ['Anti-microbial', 'Adaptogenic', 'Anti-inflammatory'],
     traditionalUses: [
       'Respiratory conditions',
-      'Stress management', 
+      'Stress management',
       'Immune support',
       'Fever reduction'
     ],
-    category: [PlantCategory.HERBS, PlantCategory.COOLING, PlantCategory.ALL]
+    category: [PlantCategory.HERBS, PlantCategory.ALL]
   },
   {
-    id: '2', 
+    id: '2',
     name: 'Neem',
     scientificName: 'Azadirachta indica',
     family: 'Meliaceae',
     commonNames: ['Indian Lilac', 'Margosa'],
-    image: 'https://img.freepik.com/premium-photo/neem-leaves-branch_55883-1587.jpg',
+    image: 'https://img.freepik.com/premium-photo/close-up-fresh-green-leaves-against-white-background_1048944-18559582.jpg?w=740',
     medicinalProperties: ['Anti-bacterial', 'Anti-fungal', 'Blood purifying'],
     traditionalUses: [
       'Skin conditions',
@@ -31,15 +31,15 @@ export const plants: Plant[] = [
       'Blood purification',
       'Natural pesticide'
     ],
-    category: [PlantCategory.TREES, PlantCategory.COOLING, PlantCategory.ALL]
+    category: [PlantCategory.TREES, PlantCategory.ALL]
   },
   {
     id: '3',
     name: 'Ashwagandha',
     scientificName: 'Withania somnifera',
-    family: 'Solanaceae', 
+    family: 'Solanaceae',
     commonNames: ['Indian Ginseng', 'Winter Cherry'],
-    image: 'https://img.freepik.com/free-photo/ashwagandha-powder-bowl_1150-42984.jpg',
+    image: 'https://img.freepik.com/free-photo/composition-nutritious-cassava-roots-sliced_23-2149091025.jpg?t=st=1746721211~exp=1746724811~hmac=1a1fa3a9cdb9caf31beb0e56baa2ac76eb2e4a469002a63b4eb36b53f861c590&w=740',
     medicinalProperties: ['Adaptogenic', 'Anti-stress', 'Immune boosting'],
     traditionalUses: [
       'Stress relief',
@@ -47,7 +47,7 @@ export const plants: Plant[] = [
       'Sleep improvement',
       'Immunity support'
     ],
-    category: [PlantCategory.ROOTS, PlantCategory.WARMING, PlantCategory.ALL]
+    category: [PlantCategory.HERBS, PlantCategory.ALL]
   },
   {
     id: '4',
@@ -55,7 +55,7 @@ export const plants: Plant[] = [
     scientificName: 'Curcuma longa',
     family: 'Zingiberaceae',
     commonNames: ['Haldi', 'Indian Saffron'],
-    image: 'https://img.freepik.com/free-photo/turmeric-powder-root-black-surface_1150-42736.jpg',
+    image: 'https://img.freepik.com/free-photo/closeup-shot-turmerics-with-wooden-shovel_181624-28460.jpg?t=st=1746721267~exp=1746724867~hmac=e49dbf74116a72694295810925647462edc97c9d8c7d1ed1d38392274a901234&w=1380',
     medicinalProperties: ['Anti-inflammatory', 'Antioxidant', 'Anti-microbial'],
     traditionalUses: [
       'Joint pain',
@@ -71,7 +71,7 @@ export const plants: Plant[] = [
     scientificName: 'Bacopa monnieri',
     family: 'Plantaginaceae',
     commonNames: ['Water Hyssop', 'Herb of Grace'],
-    image: 'https://img.freepik.com/free-photo/green-leaves-plant_1150-43266.jpg',
+    image: 'https://img.freepik.com/free-photo/flowering-endemic-maltese-spurge-euphorbia-melitensis-shrub_181624-21549.jpg?t=st=1746721400~exp=1746725000~hmac=1c5dfd9333e58282546c9876198505fe1eb6c0c0c6650855e36656c8953867d7&w=1380',
     medicinalProperties: ['Memory enhancing', 'Anti-anxiety', 'Neuroprotective'],
     traditionalUses: [
       'Cognitive enhancement',
@@ -83,75 +83,11 @@ export const plants: Plant[] = [
   },
   {
     id: '6',
-    name: 'Amla',
-    scientificName: 'Phyllanthus emblica',
-    family: 'Phyllanthaceae',
-    commonNames: ['Indian Gooseberry', 'Amalaki'],
-    image: 'https://img.freepik.com/free-photo/amla-fruit_1150-42950.jpg',
-    medicinalProperties: ['Antioxidant', 'Immune boosting', 'Vitamin C rich'],
-    traditionalUses: [
-      'Hair health',
-      'Eye care',
-      'Digestive health',
-      'Immunity enhancement'
-    ],
-    category: [PlantCategory.FRUITS, PlantCategory.COOLING, PlantCategory.ALL]
-  },
-  {
-    id: '7',
-    name: 'Ginger',
-    scientificName: 'Zingiber officinale',
-    family: 'Zingiberaceae',
-    commonNames: ['Adrak', 'Sunth'],
-    image: 'https://img.freepik.com/free-photo/ginger-root-powder_1150-42936.jpg',
-    medicinalProperties: ['Anti-inflammatory', 'Digestive aid', 'Circulation boosting'],
-    traditionalUses: [
-      'Nausea relief',
-      'Digestive support',
-      'Cold and flu',
-      'Joint pain'
-    ],
-    category: [PlantCategory.ROOTS, PlantCategory.WARMING, PlantCategory.ALL]
-  },
-  {
-    id: '8',
-    name: 'Aloe Vera',
-    scientificName: 'Aloe barbadensis miller',
-    family: 'Asphodelaceae',
-    commonNames: ['Ghritkumari', 'True Aloe'],
-    image: 'https://img.freepik.com/free-photo/aloe-vera-plant_1150-42984.jpg',
-    medicinalProperties: ['Skin healing', 'Anti-inflammatory', 'Moisturizing'],
-    traditionalUses: [
-      'Skin care',
-      'Burn treatment',
-      'Digestive health',
-      'Hair care'
-    ],
-    category: [PlantCategory.HERBS, PlantCategory.COOLING, PlantCategory.ALL]
-  },
-  {
-    id: '9',
-    name: 'Arjuna',
-    scientificName: 'Terminalia arjuna',
-    family: 'Combretaceae',
-    commonNames: ['White Marudah', 'Arjun'],
-    image: 'https://img.freepik.com/free-photo/tree-bark_1150-42990.jpg',
-    medicinalProperties: ['Cardioprotective', 'Anti-inflammatory', 'Antioxidant'],
-    traditionalUses: [
-      'Heart health',
-      'Blood pressure',
-      'Angina prevention',
-      'Cardiac support'
-    ],
-    category: [PlantCategory.TREES, PlantCategory.ALL]
-  },
-  {
-    id: '10',
     name: 'Shatavari',
     scientificName: 'Asparagus racemosus',
     family: 'Asparagaceae',
     commonNames: ['Indian Asparagus', 'Queen of Herbs'],
-    image: 'https://img.freepik.com/free-photo/asparagus-plant_1150-42992.jpg',
+    image: 'https://img.freepik.com/free-photo/top-view-saffron-still-life-composition_23-2149130015.jpg?t=st=1746721458~exp=1746725058~hmac=f031fa45cf35c06b7172cc2eea2d883c0e80a045611d640241ad87bf2bf48df7&w=1380',
     medicinalProperties: ['Adaptogenic', 'Galactagogue', 'Anti-inflammatory'],
     traditionalUses: [
       'Women\'s health',
